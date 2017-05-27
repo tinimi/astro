6c11bb90fbedf99b6cb6a9cb3d012ed5f8591e55
tinimi <tinimi@gmail.com>
2017-05-27 19:21:24 +0300

    Make it shorter

--- sunset.php
+++ sunset.php
@@ -1,15 +1,9 @@
 #!/usr/bin/php
 <?php
 
-define("LAT", "46.96");
-define("LON", "31.95");
-define("TZ", "Europe/Kiev");
+date_default_timezone_set("Europe/Kiev");
 
-date_default_timezone_set(TZ);
-
-$date = time();
-
-$sun_info = date_sun_info($date, LAT, LON);
+$sun_info = date_sun_info(time(), "46.96", "31.95");
 asort($sun_info);
 foreach ($sun_info as $key => $val) {
 	echo "$key: " . date("H:i:s", $val) . "\n";
